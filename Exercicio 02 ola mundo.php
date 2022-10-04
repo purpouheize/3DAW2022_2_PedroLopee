7086278b919a1d72498fdd157d1d89a8f1cd7ed4
purpouheize <60940200+purpouheize@users.noreply.github.com>
2022-10-03 22:12:53 -0300

    Add files via upload

---
+++ Exercicio 02 ola mundo.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            echo "Hello World";
+        ?>
+        <h1><?php
+            echo "Ola Mundao!";
+        ?>
+        <h1>
+    </body>
+</html>
